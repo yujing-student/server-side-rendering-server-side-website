6255c97b367f1de454a51991295d8f23776a35ba
yujing <medischdossier03@gmail.com>
2024-03-17 14:59:28 +0100

    eport weggehaald

--- app.js
+++ app.js
@@ -176,5 +176,3 @@ app.listen(app.get('port'), function () {
     // Toon een bericht in de console en geef het poortnummer door
     console.log(`Application started on http://localhost:${app.get('port')}`)
 })
-
-module.exports = app
